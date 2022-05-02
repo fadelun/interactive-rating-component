ec06a570c9f7ec1daacd0d9c9ad54622f1e7162e
MuhammadFadll <fadeljourney@gmail.com>
2022-05-02 19:47:34 +0700

    fixed my code

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const submit = document.querySelector(".btn");
 const start = document.querySelector(".state-start");
 const end = document.querySelector(".state-end");
 const result = document.querySelector(".result");
+const allRating = document.querySelectorAll(".radio-input");
 
 ratings.addEventListener("click", (e) => {
-  const allRating = document.querySelectorAll(".radio-input");
-
   //----menghilangkan class "actived" pada seluruh tag "div.radio-input"
   allRating.forEach((item) => {
     item.classList.remove("actived");
@@ -23,12 +22,12 @@ ratings.addEventListener("click", (e) => {
 });
 
 submit.addEventListener("click", () => {
-  start.classList.remove("show");
-  end.classList.add("show");
+  if (result.innerText !== "") {
+    start.classList.remove("show");
+    end.classList.add("show");
+  }
 });
 
-// console.log(result.children[0].innerText);
-
 function checkin(answer) {
-  result.children[0].innerText = answer;
+  result.innerText = `You selected ${answer} out 5`;
 }
